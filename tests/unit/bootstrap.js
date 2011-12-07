70088dbdc2a8a56c5c43401a13c6a33816b86202
Hitoshi Amano <seijro@gmail.com>
2011-12-07 04:11:57 +0000

    Initial commit

---
+++ tests/unit/bootstrap.js
@@ -0,0 +1,5 @@
+var path = require('path');
+
+var BaseFacebook = require(path.join(__dirname, '..', '..', 'lib', 'basefacebook.js'));
+var Facebook = require(path.join(__dirname, '..', '..', 'lib', 'facebook.js'));
+
