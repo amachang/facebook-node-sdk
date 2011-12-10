2a27956dcd38a6d404a9753db7c4dce5ae07e7cc
Hitoshi Amano <seijro@gmail.com>
2011-12-10 02:32:12 +0000

    Drop nodeunit tests

--- tests/unit/bootstrap.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-
-var BaseFacebook = require(path.join(__dirname, '..', '..', 'lib', 'basefacebook.js'));
-var Facebook = require(path.join(__dirname, '..', '..', 'lib', 'facebook.js'));
-
